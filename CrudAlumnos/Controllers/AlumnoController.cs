0f2734a5795dba31593dfc95ec7873d4e67f1ab4
IvanCorzo03 <ivan.corzo@globant.com>
2018-09-05 08:52:26 -0500

    commit initial

---
+++ CrudAlumnos/Controllers/AlumnoController.cs
@@ -0,0 +1,37 @@
+﻿using CrudAlumnos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudAlumnos.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        // GET: Alumno
+        public ActionResult Index()
+        {
+            AlumnoContext db = new AlumnoContext();
+
+            //Esta Condicion Me Trae La Lista De Alumnos Mayores A 18 (LinQ) 
+            //List<Alumno> lista = db.Alumno.Where(a => a.Edad > 18).ToList();
+
+            return View(db.Alumno.ToList());
+        }
+        //Encargado de llamar a la vista
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Agregar(Alumno a)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            return View();
+
+        }
+    }
+}
---
+++ CrudAlumnos/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudAlumnos.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /HelloWorld/ 
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
